3ae45dbc98533ecae13d1c6bb0715897e076bb04
Asmail <asmail790@gmail.com>
2022-11-04 03:17:54 +0100

    added login and logout functions

--- editor/forms.py
+++ editor/forms.py
@@ -4,9 +4,17 @@ from django.forms import Form
 from .models import Note
 
 class NoteForm(ModelForm):
+    
     class Meta:
+        
         model = Note
+        
         fields = ['title','text']
 
-class NameForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
+
+
+class LoginForm(forms.Form):
+    
+    username = forms.CharField(label='username', max_length=100)
+
+    password = forms.CharField(widget=forms.PasswordInput, max_length=100)
--- noteapp/settings.py
+++ noteapp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'editor.apps.EditorConfig'
+    'editor.apps.EditorConfig',
 ]
 
 MIDDLEWARE = [
