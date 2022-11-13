ac073da5f14b71322a541f85acc062e91a693dd8
Asmail <asmail790@gmail.com>
2022-11-13 01:04:54 +0100

    changed url paths for register and unregister

--- editor/urls.py
+++ editor/urls.py
@@ -2,12 +2,12 @@
 from django.urls import include, path
 from . import views
 urlpatterns = [
-    path('', views.editor, name='editor'),
+    path('', views.noteList, name='editor'),
     path('noteedit/<int:id>:', views.noteEdit, name='note_edit'),
     path('noteadd/', views.noteAdd, name='note_add'),
     path('noteDelete/<int:id>', views.noteDelete, name='note_delete'),
     path('accounts/login/', views.login_, name="login"),
     path('accounts/logout/', views.logout_view, name="logout"),
-    path('register/',views.register_account, name="register_account"),
-    path('unregister/',views.unregister_account, name="unregister_account"),
+    path('accounts/register/',views.register_account, name="register_account"),
+    path('accounts/unregister/',views.unregister_account, name="unregister_account"),
 ]
